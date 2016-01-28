3f45dcbc1ea7331e088daaa96b118877b2809992
Luis Fernando Gomes <dev@luiscoms.com.br>
2016-01-28 17:58:07 -0200

    Updated autoload

--- src/autoload.php
+++ src/autoload.php
@@ -1,29 +1,4 @@
 <?php
 
-$bootstrapPath = dirname(dirname(realpath(__FILE__))). DIRECTORY_SEPARATOR;
-
-set_include_path(
-    implode(
-        PATH_SEPARATOR,
-        array_unique(
-            array_merge(
-                array(
-                    $bootstrapPath . 'src',
-                    $bootstrapPath . 'tests',
-                ),
-                explode(PATH_SEPARATOR, get_include_path())
-            )
-        )
-    )
-);
-
-spl_autoload_register(
-    function ($class) {
-        $classPath = stream_resolve_include_path(
-            str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php'
-        );
-        if ($classPath !== false) {
-            require $classPath;
-        }
-    }
-);
+$loader = require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' .DIRECTORY_SEPARATOR . 'autoload.php';
+$loader->add('Release\\', __DIR__);
