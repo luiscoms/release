a5e7665c64760790e2132f6d6fbfa100a0e7917f
Luis Fernando Gomes <dev@luiscoms.com.br>
2016-01-29 02:14:51 -0200

    Added update major minor and patch methods

--- src/Release/Version.php
+++ src/Release/Version.php
@@ -4,8 +4,44 @@ namespace Release;
 
 class Version
 {
+    private $major;
+    private $minor;
+    private $patch;
+    private $preRelease;
+    private $build;
+
+    public function __construct($major = 0, $minor = 0, $patch = 1, $preRelease = '', $build = '')
+    {
+        $this->major = $major;
+        $this->minor = $minor;
+        $this->patch = $patch;
+        $this->preRelease = $preRelease;
+        $this->build = $build;
+    }
+
+    public function updateMajor()
+    {
+        $this->major += 1;
+        $this->minor = 0;
+        $this->patch = 0;
+    }
+
+    public function updateMinor()
+    {
+        $this->minor += 1;
+        $this->patch = 0;
+    }
+
+    public function updatePatch()
+    {
+        $this->patch += 1;
+    }
+
     public function __toString()
     {
-        return '0.0.1';
+        $preRelease = (!empty($this->preRelease) ? '-' : '') . $this->preRelease;
+        $build = (!empty($this->build) ? '+' : '') . $this->build;
+        $versionString = $this->major . '.' . $this->minor . '.'  . $this->patch . $preRelease . $build;
+        return $versionString;
     }
 }
--- tests/Release/VersionTest.php
+++ tests/Release/VersionTest.php
@@ -4,15 +4,88 @@ namespace Release;
 
 class VersionTest extends \PHPUnit_Framework_TestCase
 {
+    public function versionsProvider()
+    {
+        // MAJOR.MINOR.PATCH pre-release Build
+        return array(
+            array(0, 0, 1, 'alpha', ''),
+            array(0, 1, 1, 'alpha.1', '123'),
+            array(1, 0, 1, 'beta', '33'),
+            array(1, 1, 3, '', ''),
+            array(2, 0, 3, 'beta.11', ''),
+            array(1, 0, 0, 'rc.1', ''),
+            array(0, 4, 7, 'rc.21', '33'),
+            array(1, 11, 33, '0.3.7', 20130313144700),
+            array(4, 8, 55, 'x.7.z.92', 'exp.sha.5114f85')
+        );
+    }
+
     protected function assertPreConditions()
     {
         $this->assertTrue(class_exists("Release\Version"), 'Version class does not exists');
     }
 
-    public function testVersionDefault()
+    public function testDefaultVersion()
     {
         $version = new Version();
 
         $this->assertEquals('0.0.1', $version);
+    }
+
+    /**
+     * @dataProvider versionsProvider
+     */
+    public function testToString($major, $minor, $patch, $preRelease, $build)
+    {
+        $version = new Version($major, $minor, $patch, $preRelease, $build);
+        $preRelease = (!empty($preRelease) ? '-' : '') . $preRelease;
+        $build = (!empty($build) ? '+' : '') . $build;
+        $versionString = $major . '.' . $minor . '.' . $patch . $preRelease . $build;
+        $this->assertEquals($versionString, $version);
+    }
+
+    /**
+     * @dataProvider versionsProvider
+     */
+    public function testUpdateMajor($major, $minor, $patch, $preRelease, $build)
+    {
+        $version = new Version($major, $minor, $patch, $preRelease, $build);
+        $version->updateMajor();
+
+        $preRelease = (!empty($preRelease) ? '-' : '') . $preRelease;
+        $build = (!empty($build) ? '+' : '') . $build;
+        $versionString = ($major + 1) . '.0.0' . $preRelease . $build;
+
+        $this->assertEquals($versionString, $version);
+    }
+
+    /**
+     * @dataProvider versionsProvider
+     */
+    public function testUpdateMinor($major, $minor, $patch, $preRelease, $build)
+    {
+        $version = new Version($major, $minor, $patch, $preRelease, $build);
+        $version->updateMinor();
+
+        $preRelease = (!empty($preRelease) ? '-' : '') . $preRelease;
+        $build = (!empty($build) ? '+' : '') . $build;
+        $versionString = $major . '.' . ($minor + 1) . '.0' . $preRelease . $build;
+
+        $this->assertEquals($versionString, $version);
+    }
+
+    /**
+     * @dataProvider versionsProvider
+     */
+    public function testUpdatePatch($major, $minor, $patch, $preRelease, $build)
+    {
+        $version = new Version($major, $minor, $patch, $preRelease, $build);
+        $version->updatePatch();
+
+        $preRelease = (!empty($preRelease) ? '-' : '') . $preRelease;
+        $build = (!empty($build) ? '+' : '') . $build;
+        $versionString = $major . '.' . $minor . '.' . ($patch + 1) . $preRelease . $build;
+
+        $this->assertEquals($versionString, $version);
     }
 }
