8e511ef44566af3a742eaf7e49478c7eb3263575
Luis Fernando Gomes <dev@luiscoms.com.br>
2016-02-05 01:51:43 -0200

    Removed unused class

--- src/Release/Command/Current.php
+++ src/Release/Command/Current.php
@@ -4,7 +4,6 @@ namespace Release\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Current extends Command
