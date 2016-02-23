c2465e102a482d83978e69c74b27765701486591
Luis Fernando Gomes <dev@luiscoms.com.br>
2016-02-22 23:12:52 -0300

    Added runtests script

---
+++ runtests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BIN_DOCKER=$(which docker)
+
+if [ $? != 0 ]; then
+    echo "Unable to find docker"
+    exit 1
+fi
+
+docker run -it -v ${PWD}:/release luiscoms/release chmod 444 tests/fixtures/invalid/perms/*/composer.json
+docker run -it -v ${PWD}:/release luiscoms/release phpunit -vc tests/
