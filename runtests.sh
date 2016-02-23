30e9350283e325e9d90ca4c1f53adb6b9a2e65fb
Luis Fernando Gomes <dev@luiscoms.com.br>
2016-02-23 14:11:33 -0300

    Improved testdox output

--- runtests.sh
+++ runtests.sh
@@ -7,5 +7,22 @@ if [ $? != 0 ]; then
     exit 1
 fi
 
-docker run -it -v ${PWD}:/release luiscoms/release chmod 444 tests/fixtures/invalid/perms/*/composer.json
-docker run -it -v ${PWD}:/release -u `stat . -c "%u:%g"` luiscoms/release phpunit -vc tests/
+PJ_ROOT=$(pwd)
+
+if ! [ -d vendor ]; then
+    echo "Running composer install"
+    docker run --rm -it -v ${PJ_ROOT}:/release -u `stat . -c "%u:%g"` luiscoms/release composer install
+fi
+
+docker run --rm -it -v ${PJ_ROOT}:/release luiscoms/release chmod 444 tests/fixtures/invalid/perms/*/composer.json
+docker run --rm -it -v ${PJ_ROOT}:/release -u `stat . -c "%u:%g"` luiscoms/release phpunit --testdox -vc tests/
+
+if [ -z $1 ];then
+    exit;
+fi
+
+docker run --rm -it \
+            -e "COVERALLS_RUN_LOCALLY=1" \
+            -e "COVERALLS_REPO_TOKEN="$COVERALLS_REPO_TOKEN \
+            -v ${PJ_ROOT}:/release luiscoms/release \
+            vendor/bin/coveralls -v
