37f21fd73f7a0932de14d5c07d43b5fa2fafcdec
Luis Fernando Gomes <dev@luiscoms.com.br>
2016-02-25 11:09:21 -0300

    Removed unused import

--- src/Release/Command/Bump.php
+++ src/Release/Command/Bump.php
@@ -3,7 +3,6 @@
 namespace Release\Command;
 
 use Release\Handler\HandlerFactory;
-use Release\IO\ComposerIO;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
